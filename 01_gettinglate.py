4adf31eb51d6ce7a549d5c699a132cb3b6bf1b3f
Paweł Zarembski <pawezarem@gmail.com>
2018-07-07 23:09:58 +0200

    changed plots

--- 01_gettinglate.py
+++ 01_gettinglate.py
@@ -5,6 +5,10 @@ import pandas as pd
 
 ## importowanie danych z pliku csv
 df = pd.read_csv("../../data/gettinglate.csv")
+colorTheme = '#bbff47'
+colorSecondary = '#fff4a8'
+colorPawel = '#a81212'
+colorKamil = '#084c7a'
 
 ## zmiana tabeli
 #  dodanie sekund do minut
@@ -24,80 +28,127 @@ df = df.drop('Lp', axis=1)
 df.columns = ['weekday', 'day', 'month', 'onhour', 'timep', 'presp', 'timek', 'presk']
 df.to_csv("../../data/gettinglate_new.csv")
 
-
-def plot_draw(points=False, lates=True, zeroes=True, sum_all=False):
-    "Rysowanie wykresów, points - wykres punktowy ze spóźnieniami z poszczególnych dni, sum_all - wykres liniowy z sumowaniem poszczególnych dni"
-    
-    # zmienne do całki
-    if sum_all == True:
-        sum_p = []
-        sum_k = []
-
-    # inicjalizacja wykresu    
+def plot_points():
     fig, ax = plt.subplots()
+    # pierwszy punkt i label
+    ax.plot(0, df.at[0, 'timep'], color=colorPawel, marker='.', label="Paweł")
+    ax.plot(0, df.at[0, 'timek'], color=colorKamil, marker='+', label="Kamil")
 
-    # inicjacja dla pętli
-    if sum_all == True: # całka, pierwsza wartość
-        sum_p.append(df.at[0, 'timep'])
-        sum_k.append(df.at[0, 'timep'])
-
-    if points == True:
-        # punkty z czasem spóźnienia
-        if df.at[0, 'timep'] > 0 and lates == True:
-            ax.plot(0, df.at[0, 'timep'], 'r', label="Paweł")
-        elif df.at[0, 'timep'] <= 0 and zeroes == True:
-            ax.plot(0, -1, 'r*', label="Paweł")
-        if df.at[0, 'timek'] > 0 and lates == True:
-            ax.plot(0, df.at[0, 'timek'], 'b+', label="Kamil")
-        elif df.at[0, 'timek'] <= 0 and zeroes == True:
-            ax.plot(0, -2, 'b*', label="Kamil")
-
-    # Pętla przypisująca punkty i licząca całkę    
     for i in range(1, df.shape[0]):
-        # liczenie całki jeżeli podano parametr True
-        if sum_all == True:
-            # całka ze spóźnienia 1
-            sum_p.append(sum_p[i - 1] + df.at[i, 'timep'])
+        # punkty z czasem spóźnienia
+        ax.plot(i, df.at[i, 'timep'], color=colorPawel, marker='.') 
+        ax.plot(i, df.at[i, 'timek'], color=colorKamil, marker='+')
 
-            # całka ze spóźnienia 2
-            sum_k.append(sum_k[i - 1] + df.at[i, 'timek'])
+    # Oznaczenie osi
+    ax.set(xlabel='Data', ylabel='Czas', title='Spóźnienia')
 
-        # liczenie punktów jeżeli podano paramter True
-        if points == True:
-            # punkty z czasem spóźnienia
-            if df.at[i, 'timep'] > 0 and lates == True:
-                ax.plot(i, df.at[i, 'timep'], 'r.')
-            elif df.at[i, 'timep'] <= 0 and zeroes == True:
-                ax.plot(i, -1, 'r*')
-                
-            if df.at[i, 'timek'] > 0 and lates == True:
-                ax.plot(i, df.at[i, 'timek'], 'b+')
-            elif df.at[i, 'timek'] <= 0 and zeroes == True:
-                ax.plot(i, -2, 'b*')
-
-    if sum_all == True:
-        ax.plot(sum_p, 'r', label="Suma Paweł")
-        ax.plot(sum_k, 'b', label="Suma Kamil")
-        ax.set(xlabel='Data', ylabel='Czas (minuty)',
-                title='Liczba minut spóźnienia dla danej daty')
-
-    if points == True:
-        ax.set(xlabel='Data', ylabel='Czas (minuty)',
-                title='Spóźnienie')
-
-    # legenda
+    # Legenda
     legend = ax.legend(loc='upper left', shadow=True)
-    legend.get_frame().set_facecolor('#d6e069')
+    legend.get_frame().set_facecolor(colorTheme)
 
-    # zaznaczenie obszaru
-    #collection = collections.BrokenBarHCollection.span_where(range(0, df.shape[0]), ymin=0, ymax=70, where=df['timek'] <= 0, facecolor='green', alpha=0.5)
-    #ax.add_collection(collection)
-    # rysowanie     
-    ax.grid()
+    # Linie pomocnicze, organizacja
+    major_ticksX = np.arange(0, 61, 5)
+    major_ticksY = np.arange(0, 71, 5)
+    minor_ticksX = np.arange(0, 61, 1)
+    minor_ticksY = np.arange(0, 71, 1)
+
+    # Linie pomocnicze, wyznaczanie
+    ax.set_xticks(major_ticksX)
+    ax.set_xticks(minor_ticksX, minor=True)
+    ax.set_yticks(major_ticksY)
+    ax.set_yticks(minor_ticksY, minor=True)
+
+    # Linie pomocnicze, rysowanie
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=1)
+
     plt.show()
     return
 
-plot_draw(True, True, True, False)
+def plot_sum():
+    fig, ax = plt.subplots()
+    # zmienne do całki
+    sum_p = []
+    sum_k = []
+
+    # inicjacja dla pętli
+    sum_p.append(df.at[0, 'timep'])
+    sum_k.append(df.at[0, 'timep'])
+
+    # Pętla przypisująca punkty i licząca całkę    
+    for i in range(1, df.shape[0]):
+        # całka ze spóźnienia 1
+        sum_p.append(sum_p[i - 1] + df.at[i, 'timep'])
+        # całka ze spóźnienia 2
+        sum_k.append(sum_k[i - 1] + df.at[i, 'timek'])
+
+    # oznaczenie osi
+    ax.plot(sum_p, color=colorPawel, label="Suma Paweł")
+    ax.plot(sum_k, color=colorKamil, label="Suma Kamil")
+    ax.set(xlabel='Data', ylabel='Czas', title='Suma czasu spóźnienia')
+
+    # legenda
+    legend = ax.legend(loc='upper left', shadow=True)
+    legend.get_frame().set_facecolor(colorTheme)
+    # Linie pomocnicze, organizacja
+    major_ticksX = np.arange(0, 61, 5)
+    major_ticksY = np.arange(0, 701, 50)
+    minor_ticksX = np.arange(0, 61, 1)
+    minor_ticksY = np.arange(0, 701, 10)
+    # Linie pomocnicze, wpisywanie
+    ax.set_xticks(major_ticksX)
+    ax.set_xticks(minor_ticksX, minor=True)
+    ax.set_yticks(major_ticksY)
+    ax.set_yticks(minor_ticksY, minor=True)
+    
+    # Linie pomocnicze, rysowanie
+    ax.grid(which='minor', alpha=0.2)
+    ax.grid(which='major', alpha=1)
+
+    plt.show()
+    return
+
+def plot_present():
+
+    fig, ax = plt.subplots()
+    # ilość nie przyjść bez spóźnienia (żeby odjąć od całkowitej puli)    
+    p = 0
+    k = 0
+    for i in range(df.shape[0]):
+        if (df.at[i, 'timep'] == 0) and (df.at[i, 'presp'] == False):
+            p += 1
+
+        if (df.at[i, 'timek'] == 0) and (df.at[i, 'presk'] == False):
+            k += 1
+
+    # zmienić to
+    temp1 = df[df.timep > 0].count()
+    temp2 = df[df.timek > 0].count()
+    statsMeans = (temp1[0] / (60 - p) * 100, temp2[0] / (60 - k) * 100)
+    statsTotal = (100 - statsMeans[0], 100 - statsMeans[1])
+    ind = np.arange(2)    # the x locations for the groups
+    width = 0.2       # the width of the bars: can also be len(x) sequence
+
+    p1 = plt.bar(ind, statsMeans, width, color=[colorPawel, colorKamil])
+    p2 = plt.bar(ind, statsTotal, width, bottom=statsMeans, color=colorSecondary)
+
+    plt.xticks(ind, ('Paweł', 'Kamil'))
+    plt.yticks(np.arange(0, 101, 10))
+    
+    #legend = plt.legend((p1[0], p2[0]), ('Spóźnienie', 'Całkowity czas'))
+    #legend.get_frame().set_facecolor(colorTheme)
+    plt.show()
+    return
+ 
+#plot_points()
+plot_sum()
+#plot_present()
+
+
+# zaznaczenie obszaru
+#collection = collections.BrokenBarHCollection.span_where(range(0, df.shape[0]), ymin=0, ymax=70, where=df['timek'] <= 0, facecolor='green', alpha=0.5)
+#ax.add_collection(collection)
+
 
 ## main
 # wyznaczenie spóźnień
@@ -105,9 +156,6 @@ plot_draw(True, True, True, False)
 # late_k = np.array(calculate_late_time('mink', 'seck'))
 # wydruk pomocniczej tabeli
 # print_table(late_p, late_k)
-
-# setup wykresów
-# plot_draw(True, True, True, False)
 
 # np_df = np.array(df)
 # print(np.corrcoef(np_df[:,7], np_df[:,0]))
