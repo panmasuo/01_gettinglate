ddcb37fae25f44a6b41e46456e351f0d2acac3e1
Paweł Zarembski <pawezarem@gmail.com>
2018-07-17 14:06:42 +0200

    1.0
    +comments
    +printing stats
    +update

--- 01_gettinglate.py
+++ 01_gettinglate.py
@@ -6,7 +6,7 @@ import seaborn as sns
 
 ## importowanie danych z pliku csv
 df = pd.read_csv("../../data/gettinglate.csv")
-colorTheme = '#93d127'
+colorTheme = '#ffddc4'
 colorSecondary = '#fff4a8'
 colorPawel = '#a81212'
 colorKamil = '#084c7a'
@@ -24,17 +24,22 @@ df.loc[df['secp'] != True, 'secp'] = False
 df.loc[df['seck'] >= 0, 'seck']  = True
 df.loc[df['seck'] != True, 'seck'] = False
 
-# usunięcie pierwszej kolumny i przemianowanie
+# usunięcie pierwszej kolumny, przemianowanie i zapisanie tabeli
 df = df.drop('Lp', axis=1)
 df.columns = ['weekday', 'day', 'month', 'onhour', 'timep', 'presp', 'timek', 'presk']
-df.to_csv("../../data/gettinglate_new.csv")
+df.to_csv("gettinglate_new.csv")
 
-def plot_points():
+# lista różnych ważnych statystyk
+stats = {'Spawacz' : pd.Series([df['timep'].sum(), df['timep'].mean(), 0., df['timep'].median(), df['timep'].std()], index=['Suma', 'Średnia', 'Procent', 'Mediana', 'Standard']),
+    'Bandzior' : pd.Series([df['timek'].sum(), df['timek'].mean(), 0., df['timek'].median(), df['timek'].std()], index=['Suma', 'Średnia', 'Procent', 'Mediana', 'Standard'])}
+dfstats = pd.DataFrame(stats)
+
+def plot_points():  
     "Rysowanie wykresu z punktami, które oznaczają czas spóźnenia w minutach"
     fig, ax = plt.subplots()
     # pierwszy punkt i label
-    ax.plot(0, df.at[0, 'timek'], color=colorKamil, marker='P', markersize=12, label="Kamil")
-    ax.plot(0, df.at[0, 'timep'], color=colorPawel, marker='.', markersize=14, label="Paweł")
+    ax.plot(0, df.at[0, 'timek'], color=colorKamil, marker='P', markersize=12, label="Bandzior")
+    ax.plot(0, df.at[0, 'timep'], color=colorPawel, marker='.', markersize=14, label="Spawacz")
 
     # punkty z czasem spóźnienia
     for i in range(1, df.shape[0]):
@@ -45,8 +50,8 @@ def plot_points():
     ax.set(xlabel='Data', ylabel='Czas', title='Spóźnienia')
 
     # Linie średniej
-    ax.hlines(13.16, 0, df.shape[0], color=colorPawel)
-    ax.hlines(7.18, 0, df.shape[0], color=colorKamil)
+    ax.hlines(13.16, 0, df.shape[0] - 1, color=colorPawel)
+    ax.hlines( 7.18, 0, df.shape[0] - 1, color=colorKamil)
 
     # Legenda
     legend = ax.legend(loc='upper left', shadow=True)
@@ -72,10 +77,13 @@ def plot_points():
     ax.grid(which='minor', alpha=0.2)
     ax.grid(which='major', alpha=1)
 
+    manager = plt.get_current_fig_manager()
+    manager.resize(*manager.window.maxsize())
     plt.show()
     return
 
 def plot_sum():
+    "Rysowanie wykresu z całką spóźnienia"
     fig, ax = plt.subplots()
     # zmienne do całki
     sum_p = []
@@ -93,8 +101,8 @@ def plot_sum():
         sum_k.append(sum_k[i - 1] + df.at[i, 'timek'])
 
     # oznaczenie osi
-    ax.plot(sum_p, color=colorPawel, label="Suma Paweł")
-    ax.plot(sum_k, color=colorKamil, label="Suma Kamil")
+    ax.plot(sum_p, color=colorPawel, label="Suma Spawacz")
+    ax.plot(sum_k, color=colorKamil, label="Suma Bandzior")
     ax.set(xlabel='Data', ylabel='Czas', title='Suma czasu spóźnienia')
 
     # legenda
@@ -120,11 +128,13 @@ def plot_sum():
     ax.grid(which='minor', alpha=0.2)
     ax.grid(which='major', alpha=1)
 
+    manager = plt.get_current_fig_manager()
+    manager.resize(*manager.window.maxsize())
     plt.show()
     return
 
 def plot_present():
-
+    "Rysowanie wykresu słupkowego z procentem spóźnień"
     fig, ax = plt.subplots()
     # ilość nie przyjść bez spóźnienia (żeby odjąć od całkowitej puli)    
     p = 0
@@ -139,10 +149,15 @@ def plot_present():
     # zmienić to
     temp1 = df[df.timep > 0].count()
     temp2 = df[df.timek > 0].count()
+    # procent spóźniania
     statsMeans = (temp1[0] / (60 - p) * 100, temp2[0] / (60 - k) * 100)
+    # dorównanie do 100%
     statsTotal = (100 - statsMeans[0], 100 - statsMeans[1])
     ind = np.arange(2)    # lokacje na osi x
-    width = 0.2       # the width of the bars: can also be len(x) sequence
+    width = 0.2       # grubość paska
+    # statystyki
+    dfstats.at["Procent", "Spawacz"] = temp1[0]
+    dfstats.at["Procent", "Bandzior"] = temp2[0]
 
     # rysowanie słupków
     p1 = plt.bar(ind, statsMeans, width, color=[colorPawel, colorKamil])
@@ -154,33 +169,36 @@ def plot_present():
         plt.text(rect.get_x() + rect.get_width()/2.0, height, '%d' % int(height) + '%', ha='center', va='bottom')
 
     # podpis osi
-    plt.xticks(ind, ('Paweł', 'Kamil'))
+    plt.xticks(ind, ('Spawacz', 'Bandzior'))
     plt.yticks(np.arange(0, 101, 10))
     
     # legenda
-    legend = plt.legend((p1[0], p1[1], p2[0]), ('[%] spóźnienie Paweł', '[%] spóźnienie Kamil', 'Całkowity czas'))
+    legend = plt.legend((p1[0], p1[1], p2[0]), ('[%] spóźnienie Spawacz', '[%] spóźnienie Bandzior', 'Całkowity czas'))
     legend.get_frame().set_facecolor(colorTheme)
 
     plt.show()
     return
     
-def CorrMtx(df1, dropDuplicates = True):
-
-    # Your dataset is already a correlation matrix.
-    # If you have a dateset where you need to include the calculation
-    # of a correlation matrix, just uncomment the line below:
+def plot_corr(df, dropDuplicates = True):
+    "Rysowanie wykresu korelacji zmiennych"
+    df1 = df.copy()
+    df1 = df1.drop('presp', axis=1)
+    df1 = df1.drop('presk', axis=1)
+    df1 = df1.drop('day', axis=1)
+    df1.columns = ['Dzień tygodnia','Miesiąc', 'Godzina rozpoczęcia', 'Spóźnienie Spawacz', 'Spóźnienie Bandzior']
     df1 = df1.corr()
+    print(df1)
 
-    # Exclude duplicate correlations by masking uper right values
+    # Stworzenie macierzy trójkątnej z symetrycznej
     if dropDuplicates:    
         mask = np.zeros_like(df1, dtype=np.bool)
         mask[np.triu_indices_from(mask)] = True
 
-    # Set background color / chart style
+    # kolor tła
     sns.set_style(style = 'white')
 
     # Set up  matplotlib figure
-    f, ax = plt.subplots(figsize=(11, 9))
+    fig, ax = plt.subplots(figsize=(10, 11))
 
     # Add diverging colormap from red to blue
     cmap = sns.diverging_palette(250, 10, as_cmap=True)
@@ -189,115 +207,17 @@ def CorrMtx(df1, dropDuplicates = True):
     if dropDuplicates:
         sns.heatmap(df1, mask=mask, cmap=cmap, 
                 square=True,
-                linewidth=.5, cbar_kws={"shrink": .5}, ax=ax)
+                linewidth=.5, annot=True, annot_kws={"size":10})
     else:
         sns.heatmap(df1, cmap=cmap, 
                 square=True,
-                linewidth=.5, cbar_kws={"shrink": .5}, ax=ax)
+                linewidth=.5, annot=True, annot_kws={"size":10})
+    plt.savefig('Figure_4.png', bbox_inches='tight')
     plt.show()
     return
  
-# plot_points()
-# plot_sum()
-# plot_present()
-
-df1 = df.copy()
-df1 = df1.drop('presp', axis=1)
-df1 = df1.drop('presk', axis=1)
-df1 = df1.drop('day', axis=1)
-print(df1.corr())
-
-Var_Corr = df1.corr()
-# plot the heatmap and annotation on it
-#sns.heatmap(Var_Corr, xticklabels=Var_Corr.columns, yticklabels=Var_Corr.columns, annot=True)
-
-CorrMtx(df1, dropDuplicates = False)
-
-
-
-## inne, stare funkcje, na później
-
-# zaznaczenie obszaru
-#collection = collections.BrokenBarHCollection.span_where(range(0, df.shape[0]), ymin=0, ymax=70, where=df['timek'] <= 0, facecolor='green', alpha=0.5)
-#ax.add_collection(collection)
-
-
-## main
-# wyznaczenie spóźnień
-# late_p = np.array(calculate_late_time('minp', 'secp'))
-# late_k = np.array(calculate_late_time('mink', 'seck'))
-# wydruk pomocniczej tabeli
-# print_table(late_p, late_k)
-
-# np_df = np.array(df)
-# print(np.corrcoef(np_df[:,7], np_df[:,0]))
-
-# def print_table(late_table1, late_table2):
-#     "Drukowanie tablicy z ogólnymi danymi"
-#     sum = 0
-#     iter = 0
-#     for i in range(0, len(late_table1)):
-#         if late_table1[i] > 0:
-#             sum += late_table1[i]
-#             iter += 1
-
-#     values_1 = {'Suma(min)': sum, '(h)': sum/60, 'Ilość': iter, 'Procent': iter/len(late_table1)*100,
-#             'Średnio': sum/iter}
-
-#     sum = 0
-#     iter = 0
-#     for i in range(0, len(late_table2)):
-#         if late_table2[i] > 0:
-#             sum += late_table2[i]
-#             iter += 1
-#     values_2 = {'Suma(min)': sum, '(h)': sum/60, 'Ilość': iter, 'Procent': iter/len(late_table2)*100,
-#             'Średnio': sum/iter}
-            
-#     print('Imię\tMinuty\tGodziny\tIlość\tProcent\tŚrednio')
-#     print('Paweł\t{0[Suma(min)]:0.0f}min\t{0[(h)]:0.1f}h\t{0[Ilość]:d}\t{0[Procent]:0.1f}%\t{0[Średnio]:0.1f}min '.format(values_1))
-#     print('Kamil\t{0[Suma(min)]:0.0f}min\t{0[(h)]:0.1f}h\t{0[Ilość]:d}\t{0[Procent]:0.1f}%\t{0[Średnio]:0.1f}min '.format(values_2))
-
-#     return
-
-
-# def calculate_late_time(minutes, seconds):
-#     "Zwraca listę spóźnień w kolejnych dniach. Obliczenie czasu spóźninia na podstawie minut i sekund dla danego użytkownika"
-#     time = []
-#     for i in range(0, (df['Lp'].size)):
-#         if df[minutes][i] == -1:     # -1 oznacza nie przyjście na pierwsze zajęcia, następne bez spóźnienia
-#             time.append(-1)
-#         elif df[minutes][i] >= 0:    # normalny pomiar spóźnienia                     
-#             value = df[minutes][i] + (df[seconds][i] / 60)
-#             time.append(value)
-#         else:                       # spóźnienie na następne zajęcia
-#             value = (df[minutes][i] + (df[seconds][i] / 60)) * -1
-#             time.append(value)
-#     return time
-
-
-
-    # while i < df.shape[0] - 1:
-    #     while prev < df.at[i, 'weekday']:
-    #         sumWeekP += df.at[i, 'timep']
-    #         sumWeekK += df.at[i, 'timek']
-    #         days += 1
-    #         prev = df.at[i, 'weekday']
-    #         i += 1
-
-    #     ax.hlines(sumWeekP / days, i - days, i - 1, color=colorPawel)
-    #     ax.hlines(sumWeekK / days, i - days, i - 1, color=colorKamil)
-    #     avgWeekP.append(sumWeekP / days)
-    #     avgWeekK.append(sumWeekK / days)
-    #     prev = df.at[i, 'weekday']
-    #     sumWeekP = df.at[i, 'timep']
-    #     sumWeekK = df.at[i, 'timek']
-    #     days = 1
-    #     i += 1
-    
-    # prev = df.at[0, 'weekday']
-    # sumWeekP = df.at[0, 'timep']
-    # sumWeekK = df.at[0, 'timek']
-    # days = 1
-    # avgWeekP = []
-    # avgWeekK = []
-    # i = 1
+plot_points()
+plot_sum()
+plot_present()
+plot_corr(df, dropDuplicates=False)
+print(dfstats)
